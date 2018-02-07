d8ced0344eb0efcbc6d751009cb85b3acfac19ed
andrey.radzkov <andrey.radzkov@gmail.com>
2018-02-07 10:42:39 +0300

    syntax sugar

--- src/actions/supplierActions.js
+++ src/actions/supplierActions.js
@@ -6,26 +6,22 @@ import {
 } from "../constants/actionTypes";
 import {securedGet} from "../oauth2/xhr";
 
-export const loadSupplier = (id) => {
-  return function (dispatch) {
-    securedGet(process.env.API_URL + '/resource/get-supplier/' + id).then(
-      response => {
-        dispatch({type: LOAD_SUPPLIER, data: response.data});
-      }).catch(error => {
-      return error;
-    });
-  };
+export const loadSupplier = (id) => (dispatch) => {
+  securedGet(process.env.API_URL + '/resource/get-supplier/' + id).then(
+    response => {
+      dispatch({type: LOAD_SUPPLIER, data: response.data});
+    }).catch(error => {
+    return error;
+  });
 };
 
-export const loadSuppliers = () => {
-  return function (dispatch) {
-    securedGet(process.env.API_URL + '/resource/get-suppliers/', dispatch,
-      "supplierLayer").then(res => {
-      dispatch({type: LOAD_SUPPLIERS, suppliers: res.data});
-    }).catch(error => {
-      return error;
-    });
-  };
+export const loadSuppliers = () => (dispatch) => {
+  securedGet(process.env.API_URL + '/resource/get-suppliers/', dispatch,
+    "supplierLayer").then(res => {
+    dispatch({type: LOAD_SUPPLIERS, suppliers: res.data});
+  }).catch(error => {
+    return error;
+  });
 };
 
 export const clearSupplier = () => {
@@ -41,14 +37,12 @@ export const closeModal = (modalId) => {
   return {type: CLOSE_MODAL, modalId};
 };
 
-export const closeSupplierModal = (id, history) => {
-  return function (dispatch) {
-    dispatch(closeModal("supplierModal"));
-    dispatch(clearSupplier());
-    setTimeout(function () {
-      if (id) {
-        history.push('/redux-form');
-      }
-    }, 300);
-  };
+export const closeSupplierModal = (id, history) => (dispatch) => {
+  dispatch(closeModal("supplierModal"));
+  dispatch(clearSupplier());
+  setTimeout(function () {
+    if (id) {
+      history.push('/redux-form');
+    }
+  }, 300);
 };
