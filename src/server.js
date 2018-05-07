d4ca15e9b8518d82c6b8f7f43643ed9a830f0cbe
liniyakamnya <liniyakamnya@gmail.com>
2018-05-07 21:41:07 +0300

    correct header color in browser

--- src/server.js
+++ src/server.js
@@ -48,7 +48,7 @@ class ServerSideRender extends React.Component {
         <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
         <meta charSet="utf-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
-        <meta name="theme-color" content="#E16E03"/>
+        <meta name="theme-color" content="#009688"/>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"/>
         <noscript id="jss-insertion-point"/>
         {helmet.meta.toComponent()}
