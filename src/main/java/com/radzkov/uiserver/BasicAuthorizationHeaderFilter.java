3ab408394a1d6a554bc8ceeac2ca01be506ea0d0
liniyakamnya <liniyakamnya@gmail.com>
2018-06-16 22:48:03 +0300

    add credentials

---
+++ src/main/java/com/radzkov/uiserver/BasicAuthorizationHeaderFilter.java
@@ -0,0 +1,40 @@
+package com.radzkov.uiserver;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Radzkov Andrey
+ */
+@Component
+public class BasicAuthorizationHeaderFilter extends ZuulFilter {
+
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 10;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+
+        RequestContext ctx = RequestContext.getCurrentContext();
+        String requestURL = ctx.getRequest().getRequestURL().toString();
+        if (requestURL.contains("blog")) {
+            ctx.addZuulRequestHeader("Authorization", "Basic " + "d2FzaDp3YXNocGFzc3dvcmQ=");
+        }
+        return null;
+    }
+
+}
