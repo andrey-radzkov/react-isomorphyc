8b5f453665423abe7a63b0b7ebab231e8812d111
liniyakamnya <liniyakamnya@gmail.com>
2018-06-22 21:19:30 +0300

    language detection

--- src/main/react-app/src/constants/i18n.js
+++ src/main/react-app/src/constants/i18n.js
@@ -1,18 +1,27 @@
 import i18next from "i18next/i18next";
-const i18n = i18next.init({
-  lng: "ru",
-  resources: {
-    en: {
-      translation: {
-        loginH1: "Please, Log In"
-      }
-    },
-    ru: {
-      translation: {
-        loginH1: "Пожалуйста, войдите"
+import LngDetector from 'i18next-browser-languagedetector';
+
+const detection = {
+  order: ['querystring', 'navigator', 'cookie', 'localStorage', 'htmlTag', 'path', 'subdomain'],
+  caches: ['localStorage', 'cookie'],
+};
+
+const i18n = i18next
+  .use(LngDetector)
+  .init({
+    detection: detection,
+    resources: {
+      en: {
+        translation: {
+          loginH1: "Please, Log In"
+        }
+      },
+      ru: {
+        translation: {
+          loginH1: "Пожалуйста, войдите"
+        }
       }
     }
-  }
-});
+  });
 
 export default i18n;
